15df2976f3d686cccf22ff5bdc524d819e86482b
Neeraj Nema <nema.neeraj1988@gmail.com>
2018-05-24 22:05:25 -0700

    Style update

--- js/containers/App/index.js
+++ js/containers/App/index.js
@@ -27,7 +27,6 @@ export default class App extends React.Component {
 
     componentDidMount = async () => {
         const movieKeys = (await this.getKey()) || [];
-        console.log({ movieKeys });
         this.setState({ favMovies: movieKeys });
         this.setState({ selectedTab: "Movies" });
         this.makeMoviesRequest();
@@ -106,13 +105,11 @@ export default class App extends React.Component {
             const movieKeys = (await this.getKey()) || [];
             if (movieKeys.includes(value)) {
                 const remaingKeys = movieKeys.filter(key => key !== value);
-                console.log({ remaingKeys });
                 const updatedValues = JSON.stringify([...new Set(remaingKeys)]);
                 await AsyncStorage.setItem("FAVORITE_MOVIES", updatedValues);
                 const prevFavMovies = (this.state.favMovies || []).filter(
                     key => key !== value
                 );
-                console.log({ prevFavMovies });
                 this.setState({ favMovies: [...new Set(prevFavMovies)] });
                 return;
             }
@@ -150,133 +147,145 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <TabNavigator>
-                <TabNavigator.Item
-                    selected={this.state.selectedTab === "Movies"}
-                    title="Movies"
-                    titleStyle={styles.TabTitleStyle}
-                    onPress={() => this.setState({ selectedTab: "Movies" })}
-                >
-                    <List
-                        containerStyle={{
-                            backgroundColor: "#fff"
+            <View style={styles.Container}>
+                <TabNavigator>
+                    <TabNavigator.Item
+                        selectedTitleStyle={styles.SelectedTabTitleStyle}
+                        selected={this.state.selectedTab === "Movies"}
+                        title="Movies"
+                        titleStyle={styles.TabTitleStyle}
+                        onPress={() => this.setState({ selectedTab: "Movies" })}
+                    >
+                        <List
+                            containerStyle={{
+                                backgroundColor: "#fff"
+                            }}
+                        >
+                            <FlatList
+                                data={this.state.data}
+                                renderItem={({ item }) => {
+                                    return (
+                                        <View>
+                                            <View style={styles.titleContainer}>
+                                                <Text style={styles.titleStyle}>
+                                                    {item.trackCensoredName}
+                                                </Text>
+                                                {this.renderFavIcon(item)};
+                                            </View>
+                                            <ListItem
+                                                subtitle={
+                                                    <View>
+                                                        <Text
+                                                            style={
+                                                                styles.ratingText
+                                                            }
+                                                        >
+                                                            <Text
+                                                                style={
+                                                                    styles.SubjectStyle
+                                                                }
+                                                            >
+                                                                Director:
+                                                            </Text>{" "}
+                                                            {item.artistName}
+                                                        </Text>
+                                                        <Text
+                                                            style={
+                                                                styles.ratingText
+                                                            }
+                                                        >
+                                                            <Text
+                                                                style={
+                                                                    styles.SubjectStyle
+                                                                }
+                                                            >
+                                                                Year:
+                                                            </Text>{" "}
+                                                            {new moment(
+                                                                item.releaseDate
+                                                            ).format(
+                                                                "MMM D, YYYY"
+                                                            )}
+                                                        </Text>
+
+                                                        <Text
+                                                            style={
+                                                                styles.ratingText
+                                                            }
+                                                            numberOfLines={3}
+                                                            ellipsizeMode={
+                                                                "tail"
+                                                            }
+                                                        >
+                                                            <Text
+                                                                style={
+                                                                    styles.SubjectStyle
+                                                                }
+                                                            >
+                                                                Description:
+                                                            </Text>{" "}
+                                                            {item.shortDescription
+                                                                ? item.shortDescription
+                                                                : item.longDescription}
+                                                        </Text>
+                                                    </View>
+                                                }
+                                                avatar={{
+                                                    uri: item.artworkUrl100
+                                                }}
+                                                avatarStyle={styles.avatarStyle}
+                                                containerStyle={{
+                                                    borderBottomWidth: 0
+                                                }}
+                                                hideChevron
+                                            />
+                                        </View>
+                                    );
+                                }}
+                                keyExtractor={item =>
+                                    item.trackCensoredName + item.releaseDate
+                                }
+                                ItemSeparatorComponent={this.renderSeparator}
+                                ListHeaderComponent={this.renderHeader}
+                                onRefresh={this.handleRefresh}
+                                refreshing={this.state.refreshing}
+                                extraData={this.state.favMovies}
+                                onEndReachedThreshold={50}
+                            />
+                        </List>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === "FavMovies"}
+                        selectedTitleStyle={styles.SelectedTabTitleStyle}
+                        title="Favorites"
+                        titleStyle={styles.TabTitleStyle}
+                        onPress={() => {
+                            this.setState({ selectedTab: "FavMovies" });
                         }}
                     >
-                        <FlatList
-                            data={this.state.data}
-                            renderItem={({ item }) => {
-                                return (
-                                    <View>
-                                        <View style={styles.titleContainer}>
-                                            <Text style={styles.titleStyle}>
-                                                {item.trackCensoredName}
-                                            </Text>
-                                            {this.renderFavIcon(item)};
-                                        </View>
-                                        <ListItem
-                                            subtitle={
-                                                <View>
-                                                    <Text
-                                                        style={
-                                                            styles.ratingText
-                                                        }
-                                                    >
-                                                        <Text
-                                                            style={
-                                                                styles.SubjectStyle
-                                                            }
-                                                        >
-                                                            Director:
-                                                        </Text>{" "}
-                                                        {item.artistName}
-                                                    </Text>
-                                                    <Text
-                                                        style={
-                                                            styles.ratingText
-                                                        }
-                                                    >
-                                                        <Text
-                                                            style={
-                                                                styles.SubjectStyle
-                                                            }
-                                                        >
-                                                            Year:
-                                                        </Text>{" "}
-                                                        {new moment(
-                                                            item.releaseDate
-                                                        ).format("MMM D, YYYY")}
-                                                    </Text>
-
-                                                    <Text
-                                                        style={
-                                                            styles.ratingText
-                                                        }
-                                                        numberOfLines={3}
-                                                        ellipsizeMode={"tail"}
-                                                    >
-                                                        <Text
-                                                            style={
-                                                                styles.SubjectStyle
-                                                            }
-                                                        >
-                                                            Description:
-                                                        </Text>{" "}
-                                                        {item.shortDescription
-                                                            ? item.shortDescription
-                                                            : item.longDescription}
-                                                    </Text>
-                                                </View>
-                                            }
-                                            avatar={{ uri: item.artworkUrl100 }}
-                                            avatarStyle={styles.avatarStyle}
-                                            containerStyle={{
-                                                borderBottomWidth: 0
-                                            }}
-                                            hideChevron
-                                            onPress={target => {
-                                                console.log({ target });
-                                            }}
-                                        />
-                                    </View>
-                                );
-                            }}
-                            keyExtractor={item =>
-                                item.trackCensoredName + item.releaseDate
-                            }
-                            ItemSeparatorComponent={this.renderSeparator}
-                            ListHeaderComponent={this.renderHeader}
-                            onRefresh={this.handleRefresh}
-                            refreshing={this.state.refreshing}
-                            extraData={this.state.favMovies}
-                            onEndReachedThreshold={50}
-                        />
-                    </List>
-                </TabNavigator.Item>
-                <TabNavigator.Item
-                    selected={this.state.selectedTab === "FavMovies"}
-                    title="Favorites"
-                    titleStyle={styles.TabTitleStyle}
-                    onPress={() => {
-                        this.setState({ selectedTab: "FavMovies" });
-                    }}
-                >
-                    <View>
-                        <Text>Hiiii</Text>
-                    </View>
-                </TabNavigator.Item>
-            </TabNavigator>
+                        <View>
+                            <Text>Hiiii</Text>
+                        </View>
+                    </TabNavigator.Item>
+                </TabNavigator>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    Container: {
+        flex: 1
+    },
     renderSeparator: {
         height: 1,
-        width: "100%",
         backgroundColor: "#CED0CE"
     },
     titleStyle: {
-        padding: "2%",
+        paddingLeft: 8,
+        paddingTop: 8,
+        paddingBottom: 4,
+        paddingRight: 12,
         fontWeight: "700",
         fontSize: 20
     },
@@ -289,18 +298,27 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     ratingText: {
-        paddingLeft: "15%",
+        paddingLeft: 45,
         color: "black"
     },
     SubjectStyle: {
         fontWeight: "bold"
     },
     avatarStyle: {
-        height: "235%",
-        width: "235%"
+        height: 80,
+        width: 80
     },
     TabTitleStyle: {
         fontSize: 25,
+        color: "gray",
+        textAlign: "center",
+        fontWeight: "600",
+        margin: 5,
+        paddingBottom: 8
+    },
+    SelectedTabTitleStyle: {
+        fontSize: 25,
+        color: "black",
         textAlign: "center",
         fontWeight: "700",
         margin: 5,
